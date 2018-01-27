63e44df925d686f52a77a75c25d81eaba05603f9
fede <fedisalgado@gmail.com>
2018-01-27 17:06:54 -0300

    primera version antenas prenden random

---
+++ Apagar Antenas/Assets/ActivarAnteas.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActivarAnteas : MonoBehaviour {
+	public Antena antenaScript;
+	public GameObject[] Antenas;
+
+	public int CantidadDeAntenas;
+	public float Cadencia;
+	void Start () {
+		Antenas = new GameObject[CantidadDeAntenas];
+		Invoke ("InicializarAntenas",1f);
+	}
+
+	public void InicializarAntenas(){
+		for(int i = 0 ; i < CantidadDeAntenas ; i++){
+			Antenas = GameObject.FindGameObjectsWithTag ("Antena");
+		}
+		InvokeRepeating ("activarAntena",0,Cadencia);
+	}
+
+	public void activarAntena(){
+		antenaScript = GetProximaAntenaScript();
+		antenaScript.ActivarAntena ();
+		
+	}
+
+
+	public Antena GetProximaAntenaScript(){
+		int antena =  Random.Range (0, CantidadDeAntenas);
+		antenaScript =  Antenas [antena].GetComponent<Antena> ();
+		int cantAntenas = Antenas.Length,
+			antenasProbadas = 0;
+		while(antenaScript.Encendida) {
+			antena =  Random.Range (0, CantidadDeAntenas);
+			antenaScript =  Antenas [antena].GetComponent<Antena> ();
+			Debug.Log ("AntenasProbadas: "+antenasProbadas.ToString ());
+			Debug.Log ("CantAntenas: "+cantAntenas.ToString ());
+
+			if(antenasProbadas == cantAntenas){
+				Debug.Log ("Holis");
+				SceneManager.LoadScene (0);
+			}
+			antenasProbadas++;
+			return antenaScript;
+		}
+		return antenaScript;
+	}
+}
---
+++ Apagar Antenas/Assets/Touch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+	Antena AntenaScript;
+	private int layerAntenas;
+	[SerializeField]private float LargoDelRayo;
+
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			Ray mousePosition = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit _Hit;
+
+			if(Physics.Raycast (mousePosition,out _Hit,100f)){
+				if (_Hit.collider.CompareTag ("Antena")) {
+					AntenaScript = _Hit.collider.GetComponent <Antena>();
+					AntenaScript.DesactivarAntena ();
+				}
+			}
+
+		}
+	}
+}
