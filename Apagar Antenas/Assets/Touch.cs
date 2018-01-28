db1bcf88fcf97939dca10656c10e0ce6734f3920
unknown <pablo-n-m@hotmail.com>
2018-01-28 02:51:00 -0300

    cantidad de toques necesarios para descativar antena incrementa con el tiempo

--- Apagar Antenas/Assets/Touch.cs
+++ Apagar Antenas/Assets/Touch.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour {
 	Antena AntenaScript;
-	private int layerAntenas;
+	private int layerAntenas,
+				vecesTocado = 0;
+	private static int cantidadDeToquesParaDesactivar = 1;
 	[SerializeField]private float LargoDelRayo;
 
+	public static void incCantidadDeToquesTemporalmente(){
+		cantidadDeToquesParaDesactivar++;
+	}
+
 	void Update () {
+
 		if (Input.GetButton ("Fire1")) {
 			Ray mousePosition = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit _Hit;
 
 			if(Physics.Raycast (mousePosition,out _Hit,100f)){
-				if (_Hit.collider.CompareTag ("Antena")) {
-					AntenaScript = _Hit.collider.GetComponent <Antena>();
-					AntenaScript.DesactivarAntena ();
+				if (_Hit.collider.CompareTag ("Antena") && Input.GetMouseButtonDown(0)) {
+					if (vecesTocado >= cantidadDeToquesParaDesactivar) {
+						AntenaScript = _Hit.collider.GetComponent <Antena> ();
+						AntenaScript.DesactivarAntena();
+						Debug.Log("veces tocado: " + vecesTocado);
+						Debug.Log("cantidad para desactivar: " + cantidadDeToquesParaDesactivar);
+						vecesTocado = 0;
+					} else {
+						vecesTocado++;
+					}
 				}
 			}
 
